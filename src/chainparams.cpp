2d6cdf02c9a9e5ceb4f72364f81fce4c34e6c856
Chandra Das <chandradas@Chandras-MacBook-Air.local>
2015-04-11 14:50:01 -0400

    Change base58 PUBKEY_ADDRESS

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -158,7 +158,7 @@ public:
         vSeeds.push_back(CDNSSeedData("solaristats.com", "seed.solaristats.com"));
         vSeeds.push_back(CDNSSeedData("xf2.org", "bitseed.xf2.org"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
